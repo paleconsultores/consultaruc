de9331759a63661f446df0adfe7036ed965878e8
royexr <royexrc@gmail.com>
2019-10-09 10:11:04 -0500

    responsive menu v1

--- src/components/App/Home/Home.js
+++ src/components/App/Home/Home.js
@@ -1,7 +1,11 @@
+//Dependencies
 import React from 'react';
+
+//Resources
 import { Header } from './1-Header/Header';
 import { Body } from './2-Body';
 import { Footer } from './3-Footer/Footer';
+import './Home.css'
 
 export class Home extends React.Component{
   render() {
